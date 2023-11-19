23f9bb66990baaed2cfc66f637521e83a36e1b81
Jennifer Takagi <jennifer.takagi@personio.de>
2023-11-19 21:49:54 +0100

    [feat] Render props for custom option

--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React,  { useEffect, useRef, useState } from 'react'
+import React,  { useState, useRef, useEffect } from 'react'
 
 import Text from '../../atoms/Text'
 
@@ -7,24 +7,29 @@ type SelectOption = {
   value: string
 }
 
+type RenderOptionProps = {
+  isSelected: boolean
+  getOptionRecommendedProps: (overrideProps?: Object) => Object
+  option: SelectOption
+}
+
 type SelectProps = {
   label?: string
   onOptionSelected?: (option: SelectOption, optionIndex: number) => void
   options?: SelectOption[]
+  renderOption?: (props: RenderOptionProps) => React.ReactNode
 }
 
-const Select = ({ options = [], label = 'Please select an option ...', onOptionSelected: handler }: SelectProps) => {
+const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'Please select an option ...', onOptionSelected: handler, renderOption }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
-  const [selectedIndex, setSelectedIndex] = useState<null|number>(null);
-
+  const [selectedIndex, setSelectedIndex] = useState<null|number>(null)
   const labelRef = useRef<HTMLButtonElement>(null);
-  const [overlayTop, setOverlayTop] = useState<number>(0);
+  const [overlayTop, setOverlayTop] = useState<number>(0)
 
   const onOptionSelected = (option: SelectOption, optionIndex: number) => {
-    handler && handler(option, optionIndex);
-
-    setSelectedIndex(optionIndex);
-    setIsOpen(false);
+    handler && handler(option, optionIndex)
+    setSelectedIndex(optionIndex)
+    setIsOpen(false)
   }
 
   const onLabelClick = () => {
@@ -33,19 +38,20 @@ const Select = ({ options = [], label = 'Please select an option ...', onOptionS
 
   useEffect(() => {
     setOverlayTop((
-        labelRef.current?.offsetHeight || 0
+      labelRef.current?.offsetHeight || 0
     ) + 10)
   }, [labelRef.current?.offsetHeight])
 
-  let selectedOption = null;
+  let selectedOption = null
 
   if (selectedIndex !== null) {
-    selectedOption = options[selectedIndex];
+    selectedOption = options[selectedIndex]
   }
 
   return <div className='dse-select'>
-    <button  ref={labelRef} className='dse-select__label' onClick={() => onLabelClick()}>
+    <button ref={labelRef} className='dse-select__label' onClick={() => onLabelClick()}>
       <Text>{selectedOption === null ? label : selectedOption.label}</Text>
+
       <svg className={`dse-select__caret ${isOpen ? 'dse-select__caret--open' : 'dse-select__caret--closed'}`} width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7" /></svg>
     </button>
 
@@ -53,6 +59,23 @@ const Select = ({ options = [], label = 'Please select an option ...', onOptionS
       <ul style={{ top: overlayTop }} className='dse-select__overlay'>
         {options.map((option, optionIndex) => {
           const isSelected = selectedIndex === optionIndex
+
+          const renderOptionProps = {
+            option,
+            isSelected,
+            getOptionRecommendedProps: (overrideProps = {}) => {return {
+              className: `dse-select__option
+                  ${isSelected ? 'dse-select__option--selected' : ''}
+              `,
+              key: option.value,
+              onClick: () => onOptionSelected(option, optionIndex),
+              ...overrideProps
+            }}
+          }
+
+          if (renderOption) {
+            return renderOption(renderOptionProps)
+          }
 
           return <li
             className={`dse-select__option
@@ -67,8 +90,8 @@ const Select = ({ options = [], label = 'Please select an option ...', onOptionS
               {isSelected ? (
                 <svg width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M5 13l4 4L19 7" /></svg>
               ) : null}
-              </li> 
-          })}
+            </li>
+        })}
       </ul>
     ) : null}
   </div>
