908bfc4fb06b2bb9f5e82add012b2315981595a5
Jennifer Takagi <jennifer.takagi@personio.de>
2023-11-21 16:26:17 +0100

    [feat] Control menu items with keyboard

--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -1,6 +1,12 @@
-import React,  { useState, useRef, useEffect } from 'react'
+import React,  { useState, useRef, useEffect, KeyboardEventHandler, createRef } from 'react'
 
 import Text from '../../atoms/Text'
+
+const KEY_CODES = {
+  ENTER: 13,
+  SPACE: 32,
+  DOWN_ARROW: 40
+}
 
 type SelectOption = {
   label: string
@@ -21,10 +27,12 @@ type SelectProps = {
 }
 
 const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'Please select an option ...', onOptionSelected: handler, renderOption }) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-  const [selectedIndex, setSelectedIndex] = useState<null|number>(null)
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedIndex, setSelectedIndex] = useState<null|number>(null);
+  const [highlightedIndex, setHighlightedIndex] = useState<null|number>(null);
   const labelRef = useRef<HTMLButtonElement>(null);
-  const [overlayTop, setOverlayTop] = useState<number>(0)
+  const [optionRefs, setOptionRefs] = useState<React.RefObject<HTMLLIElement>[]>([]);
+  const [overlayTop, setOverlayTop] = useState<number>(0);
 
   const onOptionSelected = (option: SelectOption, optionIndex: number) => {
     handler && handler(option, optionIndex)
@@ -48,65 +56,95 @@ const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'P
     selectedOption = options[selectedIndex]
   }
 
-  return <div className='dse-select'>
-    <button 
-      aria-controls='dse-select-list' 
-      aria-expanded={isOpen ? true: undefined} 
-      aria-haspopup={true} 
-      className='dse-select__label' 
-      onClick={() => onLabelClick()}
-      ref={labelRef} 
-    >
-      <Text>{selectedOption === null ? label : selectedOption.label}</Text>
+  const highlightItem = (optionIndex: number|null) => {
+    setHighlightedIndex(optionIndex)
+  }
 
-      <svg className={`dse-select__caret ${isOpen ? 'dse-select__caret--open' : 'dse-select__caret--closed'}`} width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7" /></svg>
-    </button>
+  const onButtonKeyDown: KeyboardEventHandler = (event) => {
+    event.preventDefault()
+
+    if ([KEY_CODES.ENTER, KEY_CODES.SPACE, KEY_CODES.DOWN_ARROW].includes(event.keyCode)) {
+      setIsOpen(true)
+
+      // set focus on the list item
+      highlightItem(0)
+    }
+  }
+
+  useEffect(() => {
+    setOptionRefs(options.map(_ => createRef<HTMLLIElement>()))
+  }, [options.length])
+
+  useEffect(() => {
+    if (highlightedIndex !== null && isOpen) {
+      const ref = optionRefs[highlightedIndex]
+
+      if (ref && ref.current) {
+        ref.current.focus()
+      }
+    }
+  }, [isOpen])
+
+  return (
+    <div className='dse-select'>
+      <button 
+        aria-controls='dse-select-list' 
+        aria-haspopup={true} 
+        aria-expanded={isOpen ? true: undefined} 
+        className='dse-select__label'
+        onClick={() => onLabelClick()}
+        onKeyDown={onButtonKeyDown}
+        ref={labelRef} 
+      >
+        <Text>{selectedOption === null ? label : selectedOption.label}</Text>
+        <svg className={`dse-select__caret ${isOpen ? 'dse-select__caret--open' : 'dse-select__caret--closed'}`} width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7" /></svg>
+      </button>
 
     {isOpen ? (
-      <ul 
-        className='dse-select__overlay'
-        id='dse-select-list'
-        role='menu' aria-hidden={isOpen ? undefined : false} 
-        style={{ top: overlayTop }} 
-      >
+      <ul role='menu' aria-hidden={isOpen ? undefined : false} id='dse-select-list' style={{ top: overlayTop }} className='dse-select__overlay'>
         {options.map((option, optionIndex) => {
           const isSelected = selectedIndex === optionIndex
+          const isHighlighted = highlightedIndex === optionIndex
+
+          const ref = optionRefs[optionIndex]
 
           const renderOptionProps = {
+            ref,
             option,
             isSelected,
             getOptionRecommendedProps: (overrideProps = {}) => {return {
-              className: `dse-select__option
-                  ${isSelected ? 'dse-select__option--selected' : ''}
-              `,
-              key: option.value,
-              onClick: () => onOptionSelected(option, optionIndex),
-              ...overrideProps
+                ref,
+                tabIndex: isHighlighted ? -1 : 0,
+                onMouseEnter: () => highlightItem(optionIndex),
+                onMouseLeave: () => highlightItem(null),
+                className: `dse-select__option
+                    ${isSelected ? 'dse-select__option--selected' : ''}
+                    ${isHighlighted ? 'dse-select__option--highlighted' : ''}
+                `,
+                key: option.value,
+                onClick: () => onOptionSelected(option, optionIndex),
+                ...overrideProps
             }}
-          }
+        }
 
-          if (renderOption) {
+        if (renderOption) {
             return renderOption(renderOptionProps)
-          }
+        }
 
-          return <li
-            className={`dse-select__option
-              ${isSelected ? 'dse-select__option--selected' : ''}
-            `}
-            onClick={() => onOptionSelected(option, optionIndex)}
-            key={option.value}>
-              <Text>
-                {option.label}
-              </Text>
+        return <li
+            {...renderOptionProps.getOptionRecommendedProps()}
+            >
+              <Text>{option.label}</Text>
 
               {isSelected ? (
-                <svg width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M5 13l4 4L19 7" /></svg>
+                  <svg width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M5 13l4 4L19 7" /></svg>
               ) : null}
             </li>
         })}
       </ul>
-    ) : null}
-  </div>
+  ) : null}
+    </div>
+  )
 }
 
 export default Select
